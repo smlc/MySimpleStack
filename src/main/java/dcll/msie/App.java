aa78d5068502f9076becc3aa4d6f063179649064
smlc <siesa.marlon@hotmail.fr>
2016-02-12 16:51:29 +0100

    Commit ignore

---
+++ src/main/java/dcll/msie/App.java
@@ -0,0 +1,13 @@
+package dcll.msie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
